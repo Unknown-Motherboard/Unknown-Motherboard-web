eb87c831411fe1da306fcdd0fe91b3916e32a350
Ian Zeid <ian_j_z@yahoo.com>
2021-03-15 14:53:20 -0400

    please test, had some weird errors on app.tsx that were on code I had not edited

--- Unknown-Motherboard-web/frontend/src/App.tsx
+++ Unknown-Motherboard-web/frontend/src/App.tsx
@@ -4,6 +4,9 @@ import './App.css';
 import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import Product from './Products/Products';
 import Home from './Home/Home';
+import Orders from './orders/Orders'
+import OrderDetail from './orders/OrderDetails';
+
 
 function App() {
   const openMenu = () => {
@@ -29,6 +32,7 @@ function App() {
                         <div className="header-links">
                             <Link to="/">Home</Link>
                             <Link to="/catalog">Catalog</Link>
+                            <Link to="/orders/">Orders</Link>
                         </div>
                     </header>
                     <aside className="sidebar">
@@ -50,6 +54,12 @@ function App() {
                             </Route>
                             <Route path="/catalog">
                                 <Product />
+                            </Route>
+                            <Route path="/orders">
+                                <Orders />
+                            </Route>
+                            <Route path="/order/:id">
+                                <OrderDetail />
                             </Route>
                         </switch>
                     </main>
