bfd2284d41abfe8d4e93ab9ef6580f22f3671cb8
mfog9 <55468475+mfog9@users.noreply.github.com>
2021-03-14 23:08:25 -0400

    Update Products.tsx

--- Unknown-Motherboard-web/frontend/src/Products/Products.tsx
+++ Unknown-Motherboard-web/frontend/src/Products/Products.tsx
@@ -5,7 +5,7 @@ function Products() {
     return(
         <div className="content">
             <ul className="products">
-                data.products.map(product  
+                {data.products.map(product =>
               <li>
                  <div className="product">
                     <img className="product-image" src={product.imageUrl} alt="product" />
@@ -16,7 +16,7 @@ function Products() {
                     <div className="product-price">{product.price}</div>
                     <div className="product-rating">{product.rating} Stars ({product.numberOfReviews} Reviews)</div>
                  </div>
-             </li>)        
+             </li>)}        
          </ul>
         </div>
     )
